24ee5e3d4869d751283565f568788e14cf6f5ec1
bhavik712 <baradbhavik41@gmail.com>
2023-07-11 16:33:52 +0530

    changes

--- tracker.js
+++ tracker.js
@@ -4,28 +4,28 @@ let resturant = {
     total_capicity : 60,
     guest_count: 0,
     checkAvailability: function (newguest){
-        let seats_left = this.total_capicity - this.guest_count
-        return newguest <= seats_left
+        let seats_left = this.total_capicity - this.guest_count;
+        return newguest <= seats_left;
     },
     addguestcount: function(newguest){
         if(resturant.checkAvailability(newguest) == true){
-            this.guest_count = this.guest_count + newguest
-            console.log(`new ${newguest} added. total ${this.guest_count} guests`)
+            this.guest_count = this.guest_count + newguest;
+            console.log(`new ${newguest} added. total ${this.guest_count} guests`);
         }
         else{
-            console.log('Sorry, No place Available')
+            console.log('Sorry, No place Available');
         }
 
     },
     removecount: function(goodbyeguest){
-        this.guest_count = this.guest_count - goodbyeguest
-        console.log(`${goodbyeguest} went. total ${this.guest_count} guests in restaurent`)
+        this.guest_count = this.guest_count - goodbyeguest;
+        console.log(`${goodbyeguest} went. total ${this.guest_count} guests in restaurent`);
 
     }
 
 }
 
 
-resturant.addguestcount(15)
-resturant.addguestcount(10)
-resturant.removecount(7)
+resturant.addguestcount(15);
+resturant.addguestcount(10);
+resturant.removecount(7);
