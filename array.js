3333aadc69240865abeb53eb66d18977c957550c
bhavik712 <baradbhavik41@gmail.com>
2023-07-11 19:06:27 +0530

    array

--- array.js
+++ array.js
@@ -1,50 +1,52 @@
 //declaring array, push,pop, splice, shift & unshift methods 
 
-const team = ['Rohit','Gill','Kohli']
-console.log(team)
-console.log(team.length)
-
+const team = ['Rohit','Gill','Kohli'];
+console.log(team);
+console.log(team.length);
 //retriving the array element
-let captain = team[0]
-console.log(captain)
+let captain = team[0];
+console.log(captain);
 
 //enter the next element at the end of array 
-team.push('Surya')
-console.log(team)
-team.push('Shreyas')
-console.log(team)
+team.push('Surya');
+console.log(team);
+team.push('Shreyas');
+console.log(team);
 
-let last = team.pop()
-console.log(last)
+let last = team.pop();
+console.log(last);
 
-let first = team.shift()
-console.log(first)
-team.unshift('Shikhar')
-console.log(team)
+let first = team.shift();
+console.log(first);
+team.unshift('Shikhar');
+console.log(team);
 
-console.log(team)
+console.log(team);
 team.splice(1,0,'Rohit') //add element at 2
-team.splice(2,0,'Pujara')
+team.splice(2,0,'Pujara');
 
 
-console.log(team)
-team.splice(3,0,'Rahane')
-console.log(team)
+console.log(team);
+team.splice(3,0,'Rahane');
+console.log(team);
 
-team[6] = 'jadeja'
-console.log(team)
+team[6] = 'jadeja';
+console.log(team);
 
-team.splice(3,3,'Kohli','Gill','Rahane')
-console.log(team)
+team.splice(3,3,'Kohli','Gill','Rahane');
+console.log(team);
 
-team.splice(2,1)
-console.log(team)
+team.splice(2,1);
+console.log(team);
 
-team.splice(2,2,'Gill','Kohli')
-console.log(team)
+team.splice(2,2,'Gill','Kohli');
+console.log(team);
 
-team.splice(2,3,'Rahane','HP')
-console.log(team)
+team.splice(2,3,'Rahane','HP');
+console.log(team);
 
-let result = team.pop()
-console.log(result)
+let result = team.pop();
+console.log(result);
+
+console.log(team.indexOf('Kohli')); //-1 for not in array
+console.log(team.indexOf('HP'));//index --3 
